e693ab4083a55722f3d1a21036d201056a1abef2
StevenChandranata <stevenchandranata10@gmail.com>
2024-12-02 14:38:04 +0700

    update dockerfile

--- app/main.py
+++ app/main.py
@@ -9,4 +9,11 @@ app.include_router(clustering_router)
 
 @app.get("/")
 def read_root():
-    return {"message": "Welcome to the Day Trip Optimization API!"}
+    return {"message": "Welcome to the Cluster API!"}
+
+if __name__ == "__main__":
+    import uvicorn
+
+    # Gunakan PORT dari environment atau default ke 8080
+    port = int(os.getenv("PORT", 8080))
+    uvicorn.run("app.main:app", host="0.0.0.0", port=port)
